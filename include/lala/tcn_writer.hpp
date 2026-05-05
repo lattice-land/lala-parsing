dee7bbbd33955c145d0da96e44751d5cbfce6fa2
multivvac <wei.huang.001@student.uni.lu>
2026-05-05 19:18:02 +0200

    Add write_preprocessed_tcn() to TCN writer
    Adds a templated write_preprocessed_tcn() that serializes a (store,
    iprop, bab) triple in the TCN text format. Store/IProp/BAB are
    duck-typed, so the helper does not depend on any concrete abstract
    domain type.

--- include/lala/tcn_writer.hpp
+++ include/lala/tcn_writer.hpp
@@ -66,6 +66,46 @@ inline std::string tcn_upper_bound_to_string(const Bound& bound) {
   return std::to_string(bound.value());
 }
 
+/** Serialize an abstract state (variable domains + ternary propagators)
+ *  to the TCN text format.
+ *
+ *  Layout:
+ *    [ "minimize x<vid>\n" | "maximize x<vid>\n" ]   # optional objective line
+ *    <num_vars>\n
+ *    <lb> <ub>\n                                     # one line per variable
+ *    ...
+ *    <num_constraints>\n
+ *    <x> <y> <op> <z>\n                              # one line per ternary bytecode
+ *    ...
+ *
+ *  Store, IProp and BAB are duck-typed: only the methods used below
+ *  (vars(), num_deductions(), load_deduce(i), is_minimization(),
+ *  is_maximization(), objective_var()) need to be available.
+ */
+template <class Store, class IProp, class BAB>
+void write_preprocessed_tcn(std::ostream& out, const Store& store, const IProp& iprop, const BAB& bab) {
+  if(bab.is_minimization()) {
+    out << "minimize x" << bab.objective_var().vid() << '\n';
+  }
+  else if(bab.is_maximization()) {
+    out << "maximize x" << bab.objective_var().vid() << '\n';
+  }
+  out << store.vars() << '\n';
+  for(int i = 0; i < store.vars(); ++i) {
+    const auto& dom = store[i];
+    out << tcn_lower_bound_to_string(dom.lb()) << ' '
+        << tcn_upper_bound_to_string(dom.ub()) << '\n';
+  }
+  out << iprop.num_deductions() << '\n';
+  for(int i = 0; i < iprop.num_deductions(); ++i) {
+    auto bytecode = iprop.load_deduce(i);
+    out << bytecode.x.vid() << ' '
+        << bytecode.y.vid() << ' '
+        << tcn_sig_to_symbol(bytecode.op) << ' '
+        << bytecode.z.vid() << '\n';
+  }
+}
+
 }
 
 #endif
